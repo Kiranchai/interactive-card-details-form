a1af78dddb447ff7f34e4bf33125037c67f24285
Norbert <sadawa.norbert@gmail.com>
2022-09-27 23:56:34 +0200

    added card number validation

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const numberInput = document.getElementById('number-input');
 const cardNumber = document.querySelector('.card-number');
+const errorColor = 'hsl(0, 100%, 66%)';
+
+function containsLetters(string){
+    return /[a-zA-Z]/.test(string);
+}
+
+function addSpace(str){
+    return str + ' ';
+}
 
 
 numberInput.addEventListener('keyup', e => {
-    cardNumber.textContent = e.target.value;
+    e.preventDefault();
+    let input = e.target.value;
+
+    cardNumber.textContent = input;
+
+    if(e.key === 'Backspace'){
+        if (cardNumber.textContent.length === 0){
+            cardNumber.textContent = '0000 0000 0000 0000';
+        }
+    }
+
+    if(e.key !== 'Backspace'){
+        if (cardNumber.textContent.replace(/ /g, '').length % 4 === 0){
+            numberInput.value = addSpace(input);
+        }
+    }
+    
 })
 
 numberInput.addEventListener('focusout', e => {
-    const number = [...e.target.value];
-
+    
+    if (containsLetters(e.target.value)){
+        numberInput.style.borderColor = errorColor;
+    } 
 });
