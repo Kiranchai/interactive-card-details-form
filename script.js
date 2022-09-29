0018c58ac43fb7f3c828373d2d150cba36eeb2e5
Norbert <sadawa.norbert@gmail.com>
2022-09-29 17:41:53 +0200

    added input validation with error popups

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const numberInput = document.getElementById('number-input');
 const cardNumber = document.querySelector('.card-number');
+const everyInput = document.querySelectorAll('.form-input');
+const lightGrayishVioletColor = 'hsl(270, 3%, 87%)';
 const errorColor = 'hsl(0, 100%, 66%)';
 
 function containsLetters(string){
@@ -10,6 +12,59 @@ function addSpace(str){
     return str + ' ';
 }
 
+function leftBlank(input){
+    return input.length === 0 ? true : false;
+}
+
+
+const createError = (body) =>{
+    const span = document.createElement('span');
+    span.innerHTML = `${body.errorMessage}`;
+    span.classList.add('error');
+    return span;
+}
+
+const isLastItemSpan = (input) => {
+    return input.parentElement.lastChild.nodeName === 'SPAN' ? true : false;
+}
+
+everyInput.forEach(input => {
+    input.addEventListener('focusout', e => {
+
+        
+
+        if(!isLastItemSpan(input) && leftBlank(e.target.value)){
+            input.style.borderColor = errorColor;
+            input.parentElement.appendChild(createError({errorMessage:`Can't be blank`}));
+            
+        }
+         else{
+            
+            if(isLastItemSpan(input) && !leftBlank(e.target.value) ){
+
+                input.parentElement.removeChild(input.parentElement.lastChild);
+                input.style.borderColor = lightGrayishVioletColor;
+                input.style.borderWidth = '1px';
+            }
+
+            
+        }
+
+        if(input.id === 'number-input'){
+            if (containsLetters(e.target.value) && !leftBlank(e.target.value)){
+                numberInput.style.borderColor = errorColor;
+                numberInput.style.borderWidth = '1px';
+                input.parentElement.appendChild(createError({errorMessage:`Wrong format, numbers only`}));
+            } 
+            else if(!containsLetters(e.target.value) && isLastItemSpan(input) && !leftBlank(e.target.value)){
+                numberInput.style.borderColor = lightGrayishVioletColor;
+                numberInput.style.borderWidth = '1px';
+                input.parentElement.removeChild(input.parentElement.lastChild);
+            }
+        }
+    })
+})
+
 
 numberInput.addEventListener('keyup', e => {
     e.preventDefault();
@@ -17,23 +72,16 @@ numberInput.addEventListener('keyup', e => {
 
     cardNumber.textContent = input;
 
-    if(e.key === 'Backspace'){
+    if(e.key == 8 || e.key == 46 || e.key === 'Backspace'){
         if (cardNumber.textContent.length === 0){
             cardNumber.textContent = '0000 0000 0000 0000';
         }
     }
 
-    if(e.key !== 'Backspace'){
+    if(e.key !== 'Backspace' && e.key !== 'Delete'){
         if (cardNumber.textContent.replace(/ /g, '').length % 4 === 0){
             numberInput.value = addSpace(input);
         }
     }
     
 })
-
-numberInput.addEventListener('focusout', e => {
-    
-    if (containsLetters(e.target.value)){
-        numberInput.style.borderColor = errorColor;
-    } 
-});
